21a47cb9c672f6fc7079f164d9368827e536fdb2
leeron <leeron6@163.com>
2023-04-29 16:01:39 +0800

    '粒子背景'

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  overflow: hidden;
+  background: #fff;
+}
+</style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="login-container">
+    <vue-particles
+        color="#fff"
+        :particleOpacity="0.7"
+        :particlesNumber="100"
+        shapeType="star"
+        :particleSize="6"
+        linesColor="#fff"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="repulse"
+    />
+
+  </div>
+</template>
+
+<script>
+/*
+vue-particles的属性：
+color: String类型。默认'#dedede'。粒子颜色。
+particleOpacity: Number类型。默认0.7。粒子透明度。
+particlesNumber: Number类型。默认80。粒子数量。
+shapeType: String类型。默认'circle'。可用的粒子外观类型有：“circle”,“edge”,“triangle”, “polygon”,“star”。
+particleSize: Number类型。默认80。单个粒子大小。
+linesColor: String类型。默认'#dedede'。线条颜色。
+linesWidth: Number类型。默认1。线条宽度。
+lineLinked: 布尔类型。默认true。连接线是否可用。
+lineOpacity: Number类型。默认0.4。线条透明度。
+linesDistance: Number类型。默认150。线条距离。
+moveSpeed: Number类型。默认3。粒子运动速度。
+hoverEffect: 布尔类型。默认true。是否有hover特效。
+hoverMode: String类型。默认true。可用的hover模式有: “grab”, “repulse”, “bubble”。
+clickEffect: 布尔类型。默认true。是否有click特效。
+clickMode: String类型。默认true。可用的click模式有: “push”, “remove”, “repulse”, “bubble”
+ */
+export default {}
+</script>
+
+<style
+    lang="scss"
+    scoped
+>
+#particles-js {
+  background: url("~@/assets/images/bg.jpeg") no-repeat center center;
+  background-size: cover;
+
+  width: 100%;
+  height: 100%;
+  position: absolute;
+}
+</style>
